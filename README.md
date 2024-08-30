96dc52a2520230136b0c769189d510e1eb69c333
Spectrumzero <lmhspecter@gmail.com>
2024-08-30 16:28:28 +0800

    0454

--- README.md
+++ README.md
@@ -14,4 +14,5 @@
 |0202|[Happy Number](https://leetcode.cn/problems/happy-number/description/)|[Hash Table](./source/cpp/unsolved/0202.cpp)|   easy   |
 |0217|[Contains Duplicate](https://leetcode.cn/problems/contains-duplicate/description/)|[Hash Table](./source/cpp/unsolved/0217.cpp); [Array, Sorting](./source/c/unsolved/0217.c)|   easy   |
 |0242|[Valid Anagram](https://leetcode.cn/problems/valid-anagram/description/)|[Hash Table](./source/cpp/unsolved/0242.cpp); [String, Sorting](./source/c/unsolved/0242.c)|   easy   |
+|0454|[4Sum II](https://leetcode.cn/problems/4sum-ii/description/)|[Hash Table](./source/cpp/unsolved/0454.cpp)|   medium   |
 |1002|[Find Common Characters](https://leetcode.cn/problems/find-common-characters/description/)|[Hash Table](./source/cpp/unsolved/1002.cpp)|   easy   |
---
+++ source/cpp/unsolved/0454.cpp
@@ -0,0 +1,39 @@
+/* 0454: 4Sum II */
+#include "../utils/common.h"
+
+class Solution {
+public:
+  /* O(n^2) */
+  // hashtable
+  int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3, vector<int> &nums4) {
+    unordered_map<int, int> countAB;
+    int res = 0;
+    // initialization
+    for (auto a : nums1) {
+      for (auto b : nums2) {
+        countAB[a + b]++;
+      }
+    }
+    // final result
+    for (auto c : nums3) {
+      for (auto d : nums4) {
+        // either 1 or 0
+        if (countAB.count(-(c + d))) {
+          // add directly
+          res += countAB[-(c + d)];
+        }
+      }
+    }
+    return res;
+  }
+};
+
+int main() {
+  vector<int> nums1 = {0, 0};
+  vector<int> nums2 = {0, 1}; // map[0] = 3; map[1] = 1
+  vector<int> nums3 = {0, -1};
+  vector<int> nums4 = {5, 0};
+  Solution sol;
+  cout << sol.fourSumCount(nums1, nums2, nums3, nums4) << endl; // 1*3+1*1=4
+  return 0;
+}
