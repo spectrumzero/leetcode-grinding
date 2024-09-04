c6f1f2d0996021502b5788876e26dac2d5685584
Spectrumzero <lmhspecter@gmail.com>
2024-09-04 20:48:16 +0800

    better 0020

--- source/cpp/0020.cpp
+++ source/cpp/0020.cpp
@@ -1,11 +1,11 @@
 /* 0020: Valid Parentheses */
 #include "./utils/common.h"
-#include <string>
 
 class Solution {
 public:
   /* O(n) */
   // stack
+  // not good structure
   bool isValid(string s) {
     stack<char> stack;
     for (auto x : s) {
@@ -24,11 +24,59 @@ public:
     }
     return stack.size() == 0;
   }
+
+  // same method, but with a better code structure
+  bool isValid2(string s) {
+    stack<char> stack;
+    for (auto x : s) {
+      if (x == '(' || x == '[' || x == '{')
+        stack.push(x);
+      else {
+        if (stack.empty())
+          return false;
+        if (x == ')' && stack.top() != '(')
+          return false;
+        if (x == ']' && stack.top() != '[')
+          return false;
+        if (x == '}' && stack.top() != '{')
+          return false;
+        stack.pop();
+      }
+    }
+    return stack.empty();
+  }
+
+  // more sustainable solution with OCP: open for extension but closed for modification.
+  // hashtable + stack
+  bool isValid3(string s) {
+    if (s.size() % 2 == 1)
+      return false;
+    stack<char> stack;
+    // use hashmap to store the parentheses pairs. key: only closed brackets.
+    unordered_map<char, char> pairs = {
+        {')', '('}, {']', '['}, {'}', '{'} // the list can go on, like {'<','>'},{'\','/'}...
+    };
+    for (auto x : s) {
+      // x is a legal key
+      if (pairs.count(x)) {
+        if (stack.empty() || stack.top() != pairs[x])
+          return false;
+        stack.pop();
+      } else {
+        stack.push(x);
+      }
+    }
+    return stack.empty();
+  }
 };
 
 int main() {
-  string s = "()";
+  string s1 = "()";    // true
+  string s2 = "{{[)}"; // false
   Solution sol;
-  cout << sol.isValid(s) << endl;
+  cout << sol.isValid3(s1) << endl;
+  cout << sol.isValid3(s2) << endl;
   return 0;
 }
+
+/* std::stack::empty(): true if empty; false if not. */
